9fae3f215db26b2e92dd1b66ac19f7b6316baaf6
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2020-03-27 14:25:09 +0530

    Update anchore.sh

--- anchore.sh
+++ anchore.sh
@@ -13,7 +13,6 @@ anchore-cli --version
 #anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image add openjdk:11-jdk
 #anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image vuln openjdk:11-jdk all
 #anchore-cli --u admin --p foobar --url http://localhost:8228/v1 registry add registry_uri access_key secret_key --registry-type=awsecr
-anchore-cli --u admin --p foobar --url http://localhost:8228/v1 registry add 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample AKIA4BS33HJOV3EIIOO7 1EH49VfVT+wPuTVeGZy6HsTFIq+hTO3L9w77ooi1 --registry-type=awsecr
 anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image add 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample:latest
 anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image vuln 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample:latest all
 sleep 15s
